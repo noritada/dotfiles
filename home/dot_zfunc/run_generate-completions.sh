4062d64735f1f909e5ab72cc4a71cdd57cf5089c
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2022-12-02 01:48:14 +0900

    Automatically generate a completion script for `rrr`

--- home/dot_zfunc/run_generate-completions.sh
+++ home/dot_zfunc/run_generate-completions.sh
@@ -5,4 +5,5 @@ set -euxo pipefail
 rustup completions zsh > _rustup
 rustup completions zsh cargo > _cargo
 mdbook completions zsh > _mdbook
+rrr completions zsh > _rrr
 chezmoi completion zsh --output="_chezmoi"
