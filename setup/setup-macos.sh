d684f3c04e3d42846531668e6e255a8d61e5fd91
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2023-08-21 16:02:19 +0900

    setup: avoid creating directories and installing software twice

--- setup/setup-macos.sh
+++ setup/setup-macos.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 # settings for screenshots
-defaults write com.apple.screencapture name ss
-mkdir ~/Desktop/screenshots
-defaults write com.apple.screencapture location ~/Desktop/screenshots
+screenshots_dir="${HOME}/Desktop/screenshots"
+if [ ! -e "${screenshots_dir}" ]; then
+    defaults write com.apple.screencapture name ss
+    mkdir "${screenshots_dir}"
+    defaults write com.apple.screencapture location "${screenshots_dir}"
+fi
 
 # disable auto swoosh (automatic space switching)
 defaults write com.apple.dock workspaces-auto-swoosh -bool NO
@@ -12,13 +15,17 @@ defaults write com.apple.dock workspaces-auto-swoosh -bool NO
 defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
 
 # install Homebrew (see https://brew.sh/index_ja )
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+if !(type "brew" > /dev/null 2>&1); then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 # install rustup (see https://rustup.rs/ )
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-rustup component add rustfmt
-rustup target add wasm32-unknown-unknown
-rustup target add x86_64-pc-windows-gnu
+if !(type "rustup" > /dev/null 2>&1); then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    rustup component add rustfmt
+    rustup target add wasm32-unknown-unknown
+    rustup target add x86_64-pc-windows-gnu
+fi
 
 # install Poetry (see https://python-poetry.org/docs/ )
 curl -sSL https://install.python-poetry.org | python3 -
