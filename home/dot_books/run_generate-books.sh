c6add6f324aaa207a78566ca9a8edd0ae335c8d0
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2023-09-03 13:27:54 +0900

    .books: clone the repository first if the directory does not exist

--- home/dot_books/run_generate-books.sh
+++ home/dot_books/run_generate-books.sh
@@ -4,11 +4,17 @@ set -euxo pipefail
 
 build_mdbook() {
     local readonly name="$1"
-    local readonly repo_path="$2"
+    local readonly repo="$2"
     local readonly subdir="$3"
 
     mkdir -p "$name"
     (
+        local readonly repo_url="https://${repo}.git"
+        local readonly repo_path="${HOME}/ghq/${repo}"
+        if [ ! -e "$repo_path" ]; then
+           ghq get "$repo_url"
+        fi
+
         cd "$repo_path"
         local readonly rev=$(git describe --tags --abbrev=0 || echo "HEAD")
         git archive --format=tar $rev
@@ -16,8 +22,8 @@ build_mdbook() {
     (cd "$name/$subdir" && mdbook build)
 }
 
-build_mdbook mdBook ~/ghq/github.com/rust-lang/mdBook guide
-build_mdbook rust-cookbook ~/ghq/github.com/rust-lang-nursery/rust-cookbook .
-build_mdbook patterns ~/ghq/github.com/rust-unofficial/patterns .
-build_mdbook rustwasm-book ~/ghq/github.com/rustwasm/book .
-build_mdbook wasm-bindgen ~/ghq/github.com/rustwasm/wasm-bindgen guide
+build_mdbook mdBook github.com/rust-lang/mdBook guide
+build_mdbook rust-cookbook github.com/rust-lang-nursery/rust-cookbook .
+build_mdbook patterns github.com/rust-unofficial/patterns .
+build_mdbook rustwasm-book github.com/rustwasm/book .
+build_mdbook wasm-bindgen github.com/rustwasm/wasm-bindgen guide
