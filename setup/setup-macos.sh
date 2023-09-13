8b49c7b29c29310d8afeb8fc359ecc0de4fe027c
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2023-09-14 00:59:01 +0900

    setup: make some operations idempotent

--- setup/setup-macos.sh
+++ setup/setup-macos.sh
@@ -2,11 +2,9 @@
 
 # settings for screenshots
 screenshots_dir="${HOME}/Desktop/screenshots"
-if [ ! -e "${screenshots_dir}" ]; then
-    defaults write com.apple.screencapture name ss
-    mkdir "${screenshots_dir}"
-    defaults write com.apple.screencapture location "${screenshots_dir}"
-fi
+defaults write com.apple.screencapture name ss
+mkdir -p "${screenshots_dir}"
+defaults write com.apple.screencapture location "${screenshots_dir}"
 
 # disable auto swoosh (automatic space switching)
 defaults write com.apple.dock workspaces-auto-swoosh -bool NO
@@ -75,10 +73,10 @@ fi
 # install rustup (see https://rustup.rs/ )
 if !(type "rustup" > /dev/null 2>&1); then
     curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path -y
-    rustup component add rustfmt
-    rustup target add wasm32-unknown-unknown
-    rustup target add x86_64-pc-windows-gnu
 fi
+rustup component add rustfmt
+rustup target add wasm32-unknown-unknown
+rustup target add x86_64-pc-windows-gnu
 
 # install Go (see https://go.dev/doc/install )
 if !(type "go" > /dev/null 2>&1); then
