a6b8f22d1800cc772cf14e6eac9c5aad1a029acd
Noritada Kobayashi <noritada.kobayashi@gmail.com>
2023-10-06 23:03:10 +0900

    setup: install Signal

--- setup/setup-macos.sh
+++ setup/setup-macos.sh
@@ -100,7 +100,7 @@ curl -L git.io/nodebrew | perl - setup
 
 # install Homebrew packages
 brew install awscli cmake coreutils eccodes exiftool ffmpeg gh git-gui hdf5 jq nkf pandoc podman procs protobuf shellcheck tmux wget
-brew install --cask brave-browser deepl drawio emacs firefox skype slack visual-studio-code vlc vscodium wireshark zed zoom
+brew install --cask brave-browser deepl drawio emacs firefox skype signal slack visual-studio-code vlc vscodium wireshark zed zoom
 
 # install tools using toolchains of Rust and Go
 cargo install --locked bat trunk
